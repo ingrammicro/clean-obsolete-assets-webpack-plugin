c4595b876b6ff17ae40c9ffcdf70fbede13ce262
Alberto del Valle <alberto.dvc@gmail.com>
2018-08-08 19:51:04 +0200

    Clean assets from s3 (issue ingrammicro/imco-webapp#708)

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict'
 
-const fs = require('fs')
-const del = require('del')
 const path = require('path')
+const AWS = require('aws-sdk')
+
+const flatMap = (f,xs) =>
+	xs.reduce((acc,x) =>
+		acc.concat(f(x)), [])
+
+Array.prototype.$$flatMap = function(f) {
+	return flatMap(f,this)
+}
 
 module.exports = CleanObsoleteAssets
 
 function CleanObsoleteAssets(options) {
-	this.previousAssets = this._getPreviousAssets(options.statsFilePath)
+	this.s3 = new AWS.S3(options.S3Config.options)
+	this.s3UploadOptions = options.S3Config.uploadOptions
+	this.bundlePath = options.bundlePath
+	this.previousAssets = this._getPreviousAssets(path.join(options.bundlePath, options.statsFileName))
 }
 
 CleanObsoleteAssets.prototype.apply = function(compiler) {
@@ -16,29 +26,37 @@ CleanObsoleteAssets.prototype.apply = function(compiler) {
 
 CleanObsoleteAssets.prototype._removeObsoleteFiles = function(compiler, compilation, done) {
 
+	console.log('Cleaning up previous bundle files')
+
 	let newAssets = compilation.getStats().toJson().assets
 		.filter(asset => asset.emitted)
 		.map(asset => asset.name)
 
-	this.previousAssets
-		.filter(asset => newAssets.indexOf(asset) == -1)
-		.map(fileName => path.join(compiler.outputPath, fileName))
-		.forEach(this._deleteFile)
+	this.previousAssets.then( assets => {
+		if (!assets || !assets.length) return Promise.resolve()
+		return Promise.all(assets.filter(asset => newAssets.indexOf(asset) == -1)
+			.$$flatMap( fileName => {
+				return [
+					this._deleteS3File(path.join(this.bundlePath, fileName)),
+					this._deleteS3File(path.join(this.bundlePath, fileName+'.gz'))
+				]
+			}))
 
-	done()
+	})
+	.then(() => done())
+	.catch( (err) => {
+		console.error(err)
+		done()
+	})
 }
 
-CleanObsoleteAssets.prototype._deleteFile = function(filePath) {
-	del.sync(filePath, {force: true})
-	console.info('Old assets file has been removed: ', filePath)
+CleanObsoleteAssets.prototype._deleteS3File = function(file) {
+	console.info(`Deleting file: ${file}`)
+	return this.s3.deleteObject({Key: file, ...this.s3UploadOptions}).promise()
 }
 
-CleanObsoleteAssets.prototype._getPreviousAssets = function(statsFilePath) {
-	var assets = []
-	try {
-		assets = JSON.parse(fs.readFileSync(statsFilePath, 'utf8', { flag : 'r' })).assets
-			.map(asset => asset.name)
-	} finally {
-		return assets
-	}
+CleanObsoleteAssets.prototype._getPreviousAssets = function(statsFile) {
+	return this.s3.getObject({Key: statsFile, ...this.s3UploadOptions}).promise()
+		.then( stats => JSON.parse(stats.Body.toString()).assets.map(asset => asset.name))
+		.catch( err => console.log('No previous bundle find'))
 }
